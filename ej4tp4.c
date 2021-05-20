0b5d3e75ac0f3183ff39487befcb8f853280fb16
kevinriberi <84488602+kevinriberi@users.noreply.github.com>
2021-05-20 17:22:31 -0300

    Update ej4tp4.c

--- ej4tp4.c
+++ ej4tp4.c
@@ -1,30 +1,18 @@
 #include <stdio.h>
 //Lexico//
-int n, i, sumatoria1, sumatoria2;
+int n, i, a, b, c;
 int main() {
 	printf("Ingrese la cantidad de números de la serie Fibonacci que quiere conocer: ");
 	scanf("%d",&n);
-	sumatoria1 = 0;
-	sumatoria2 = 1;
+	a = 0;
+	b = 1;
 	i = 1;
-	if (n==1){
-		printf("0\n");  
-	} else{
-		do {
-			printf("%d\n", sumatoria1);
-			printf("%d\n", sumatoria2);
-			sumatoria1 = sumatoria1 + sumatoria2;
-			sumatoria2 = sumatoria2 + sumatoria1;
-			i = i + 1;
-		} while (2*i < n);  //si usaba n/2, no funca porque no guarda el 0,5
-		if(n != 2){
-			if(n % 2 == 0){
-				printf("%d\n", sumatoria1);
-				printf("%d\n", sumatoria2);
-			}else{
-				printf("%d\n", sumatoria1);
-		}
-	}
-}
+	do {
+		printf("%d\n", a);
+		c = a + b;
+		a = b;
+		b = c;
+		i = i + 1;
+	} while (i <= n);  
 	return 0;
 }
