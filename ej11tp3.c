964930921618114bc70fd300495dff6987bc4449
kevinriberi <84488602+kevinriberi@users.noreply.github.com>
2021-05-20 17:33:37 -0300

    Update ej11tp3.c

--- ej11tp3.c
+++ ej11tp3.c
@@ -5,17 +5,15 @@ void main() {
 	printf("Ingrese el numero correspondiente al mes: ");
 	scanf("%d",&numeroMes);
 	if (numeroMes == 1 || numeroMes == 3 || numeroMes == 5 || numeroMes == 7 || numeroMes== 8 || numeroMes == 10 || numeroMes == 12) {
-	dias = 31;
-	} 
-	else {
+		dias = 31;
+	} else {
 		if (numeroMes == 4 || numeroMes == 6 || numeroMes == 9 || numeroMes == 11) {
-		dias = 30;
-		}
-		else {
-		dias = 28;
+			dias = 30;
+		}else {
+			dias = 28;
 		}
 
-		}
+	}
 		
 printf("Este mes tiene %d dias\n", dias);
 }
