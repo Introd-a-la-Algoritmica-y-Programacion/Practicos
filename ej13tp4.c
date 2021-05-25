6a64b5fc66fec6ed33edf5b9524b9258e770b31e
kevinriberi <84488602+kevinriberi@users.noreply.github.com>
2021-05-25 12:47:27 -0300

    Update ej13tp4.c

--- ej13tp4.c
+++ ej13tp4.c
@@ -8,11 +8,12 @@ int main() {
 	printf("Ingrese un numero entero para el exponente y:\n");
 	scanf("%d",&y);
 	calculoPotencia = 1;
+	//aca considero el caso que se utilice un exponente negativo, por eso agrego el || i <= y*-1//
 	for (i = 1; i <= y || i <= y*-1; i++){
 		calculoPotencia = calculoPotencia * a;
 	}
 	if (y < 0){
-		calculoPotencia = 1/calculoPotencia;
+		calculoPotencia = 1/calculoPotencia;   
 	}
 	printf("El resultado de elevar %d a la %d es: %.6f\n",a,y,calculoPotencia);
 	return 0;
