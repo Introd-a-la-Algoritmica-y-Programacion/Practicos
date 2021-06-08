adcd2690bd9e4098e841805ebc666e977b9b5890
kevinriberi <84488602+kevinriberi@users.noreply.github.com>
2021-06-08 18:45:13 -0300

    Add files via upload

---
+++ Kevin/TP5/ej9tp5.c
@@ -0,0 +1,31 @@
+//ej9tp5 - perteneceRecta//
+#include <stdio.h>
+//Lexico//
+float a,b,p,q;
+int estaEnLaRecta(float a,float b,float x,float y); //la uso como una variable de tipo logico
+int perteneceRecta; //variable auxiliar que almacena los valores de estaEnLaRecta
+int main() {
+	printf("Ingrese el valor del coeficiente a: \n");
+	scanf("%f", &a);
+	printf("Ingrese el valor del coeficiente b: \n");
+	scanf("%f", &b);
+	printf("Ingrese el valor de la coordenada x del punto: \n");
+	scanf("%f", &p);
+	printf("Ingrese el valor del coeficiente y del punto: \n");
+	scanf("%f", &q);
+	perteneceRecta = estaEnLaRecta(a,b,p,q);
+	printf("La funcion perteneceRecta toma el valor logico de %d\n", perteneceRecta);
+	return 0;
+}
+//fin del main//
+//implementacion de la funcion estaEnLaRecta//
+
+int estaEnLaRecta(float a,float b,float x,float y){
+int aux;
+	if(y - a*x - b == 0){
+		aux = 1;
+	}else{
+		aux = 0;
+	}
+	return aux;
+}
