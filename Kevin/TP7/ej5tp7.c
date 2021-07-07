987065525cb9efd53b3564ebcb96555931e76b7a
kevinriberi <84488602+kevinriberi@users.noreply.github.com>
2021-07-07 14:21:44 -0300

    Add files via upload

--- Kevin/TP7/ej5tp7.c
+++ Kevin/TP7/ej5tp7.c
@@ -12,7 +12,11 @@ void promedioNotas(TData notas, float *prom);
 int main(){
 	cargarNotas(&misNotas);
 	promedioNotas(misNotas, &promedio);
-	printf("El promedio de las notas cargadas es: %.2f\n", promedio);
+	if(misNotas.cant > 0){
+		printf("El promedio de las notas cargadas es: %.2f\n", promedio);
+	} else{
+		printf("No se han ingresado notas como para calcular el promedio\n");
+	}
 	return 0;
 }
 
@@ -21,12 +25,19 @@ int main(){
 void cargarNotas(TData *notas){
 	int i; //variable de control del para
 	printf("Ingrese la cantidad de notas que seran cargadas: ");
+	do{
 	scanf("%d", &notas->cant);
-	printf("Ingrese las %d notas: ", notas->cant);
-	for(i=0; i < (*notas).cant; i++){
-		do{
-			scanf("%d", &notas->a[i]);
-		} while((*notas).a[i] < 0 || (*notas).a[i] > 10);
+	if ((*notas).cant < 0 || (*notas).cant > Max){
+		printf("Ingrese una cantidad de notas valida (0 a 200): ");
+	}
+	}while((*notas).cant < 0 || (*notas).cant > Max);
+	if((*notas).cant > 0){
+		printf("Ingrese las %d notas: ", notas->cant);
+		for(i=0; i < (*notas).cant; i++){
+			do{
+				scanf("%d", &notas->a[i]);
+			} while((*notas).a[i] < 0 || (*notas).a[i] > 10);
+		}
 	}
 }
 
